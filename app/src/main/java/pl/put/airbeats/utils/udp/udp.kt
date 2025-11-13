c162a280303cd5276736b9c16c101f7105bdbecc
Borek <bartek.operacz@o2.pl>
2025-11-13 16:47:02 +0100

    fix

--- app/src/main/java/pl/put/airbeats/utils/udp/udp.kt
+++ app/src/main/java/pl/put/airbeats/utils/udp/udp.kt
@@ -7,6 +7,7 @@ import java.util.concurrent.atomic.AtomicBoolean
 import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
 import io.ktor.utils.io.ByteWriteChannel
+import io.ktor.utils.io.core.ByteReadPacket
 import io.ktor.utils.io.writeByte
 import kotlinx.io.readByteArray
 
@@ -19,17 +20,30 @@ class UdpManager {
     private val isNotClosed = AtomicBoolean(false)
     private var lastTime: Double = 0.0
     private var lastAngleY: Double = 0.0
-//    private var gyrFlag: Boolean = true
+
+    //    private var gyrFlag: Boolean = true
     private var baseAngleZ: Double = 0.0
     private val ANGLE_TRESHOLD: Double = 300.0
     private val MIN_RANGE: Double = -45.0
     private val MAX_RANGE: Double = 45.0
+    private var isLeftSensor = false;
+    private var angleX = 0.0
+    private var angleY = 0.0
+    private var angleZ = 0.0
+    private var currentTime = 0.0
+    private var deltaTime = 0.0
+    private var angleDiff = 0.0
+    private var shifted = 0.0
+    private var columnHit = 9
+    private var normalized = 0.0
+    private lateinit var datagram: Datagram
+    private lateinit var bytes: ByteArray
 
     suspend fun connectToServer(ip: String, port: Int): Boolean {
         return try {
             val selectorManager = SelectorManager(Dispatchers.IO)
 
-            if(isNotClosed.get()) {
+            if (isNotClosed.get()) {
                 tcpChannel.flushAndClose()
                 tcpSocket.close()
                 socket.close()
@@ -43,8 +57,8 @@ class UdpManager {
 
             socket = aSocket(selectorManager)
                 .udp()
-                .bind(tcpSocket.localAddress)
-//                .bind("0.0.0.0", 2138)
+//                .bind(tcpSocket.localAddress)
+                .bind("0.0.0.0", 2138)
 
             isNotClosed.set(true)
             Log.d("UdpManager", "Connected to $ip:$port")
@@ -52,12 +66,16 @@ class UdpManager {
             tcpChannel.writeByte(1)
             Log.d("UdpManager", "Send 0 to $ip:$port")
 
+            val packet = ByteReadPacket("hello".encodeToByteArray())
+            val target = InetSocketAddress(ip, port)
+            socket.send(Datagram(packet, target))
+
             val datagram = socket.receive()
             val data = datagram.packet.readByteArray()
-            val isLeftSensor = data[0].toInt() != 0
-            val angleX = data.toDoubleLE(1)
-            val angleY = data.toDoubleLE(9)
-            val angleZ = data.toDoubleLE(17)
+            isLeftSensor = data[0].toInt() != 0
+            angleX = data.toDoubleLE(1)
+            angleY = data.toDoubleLE(9)
+            angleZ = data.toDoubleLE(17)
             Log.d("UdpManager", "Received ${listOf(isLeftSensor, angleX, angleY, angleZ)}")
 
             baseAngleZ = angleZ
@@ -75,14 +93,18 @@ class UdpManager {
         isReceiving.set(true)
         while (isReceiving.get()) {
             try {
-                val datagram = socket.receive()
-                val bytes = datagram.packet.readByteArray()
+                datagram = socket.receive()
+                bytes = datagram.packet.readByteArray()
 
-                val (isLeft, pos, columnEvent) = convertControllerData(bytes)
-                Log.d("UdpManager", "converted: %s".format(listOf(isLeft, pos, columnEvent).toString()))
+//                val (isLeft, pos, columnEvent) = convertControllerData(bytes)
+                convertControllerData(bytes)
+                Log.d(
+                    "UdpManager",
+                    "converted: %s".format(listOf(isLeftSensor, normalized, columnHit).toString())
+                )
 
                 glView.queueEvent {
-                    onData(isLeft, pos, columnEvent)
+                    onData(isLeftSensor, normalized, columnHit)
                 }
 
             } catch (e: Exception) {
@@ -107,18 +129,20 @@ class UdpManager {
             .reduce { acc, v -> acc or v }
         return Double.fromBits(longBits)
     }
-    fun convertControllerData(data: ByteArray): Triple<Boolean, Double, Int> {
-        val isLeftSensor = data[0].toInt() != 0
-        val angleX = data.toDoubleLE(1)
-        val angleY = data.toDoubleLE(9)
-        val angleZ = data.toDoubleLE(17)
-        Log.d("UdpManager", "Received ${listOf(isLeftSensor, angleX, angleY, angleZ)}")
 
-        val currentTime = System.currentTimeMillis() / 1000.0
-        val deltaTime = currentTime - lastTime;
-        lastTime = currentTime;
+    fun convertControllerData(data: ByteArray) {
+        isLeftSensor = data[0].toInt() != 0
+        angleX = data.toDoubleLE(1)
+        angleY = data.toDoubleLE(9)
+        angleZ = data.toDoubleLE(17)
+//        Log.d("UdpManager", "Received ${listOf(isLeftSensor, angleX, angleY, angleZ)}")
 
-        val angleDiff = (angleY - lastAngleY) / deltaTime;
+        currentTime = (System.currentTimeMillis() / 1000.0)
+        deltaTime = currentTime - lastTime
+        lastTime = currentTime
+
+        angleDiff = (angleY - lastAngleY) / deltaTime / 10.0
+        Log.d("UdpManager", "deltaTime:$deltaTime angleDiff:$angleDiff")
         lastAngleY = angleY;
 
 
@@ -126,40 +150,41 @@ class UdpManager {
 //        if (!gyrFlag && angleDiff <= ANGLE_TRESHOLD)
 //            gyrFlag = true;
 
-        Log.d("UdpManager", "Before shift $angleZ")
-        val shifted = wrapAroundBase(angleZ);
-        Log.d("UdpManager", "After shift $shifted")
-        var columnHit = 9;
+//        Log.d("UdpManager", "Before shift $angleZ")
+        shifted = wrapAroundBase(angleZ);
+//        Log.d("UdpManager", "After shift $shifted")
+        columnHit = 9;
 
         if (/*gyrFlag && */angleDiff > ANGLE_TRESHOLD && angleY > -90.0 && angleY < 90.0) {
 //            gyrFlag = false;
 
-            if (shifted < (MIN_RANGE/2.0))
+            if (shifted < (MIN_RANGE / 2.0))
                 columnHit = 3; // right outer
-            else if (shifted >= (MIN_RANGE/2.0) && shifted < 0.0)
+            else if (shifted >= (MIN_RANGE / 2.0) && shifted < 0.0)
                 columnHit = 2; // right inner
-            else if (shifted >= 0.0 && shifted < (MAX_RANGE/2.0))
+            else if (shifted >= 0.0 && shifted < (MAX_RANGE / 2.0))
                 columnHit = 1; // left inner
             else
                 columnHit = 0; // left outer
         }
 
-        val normalized = scale(shifted)
-        Log.d("UdpManager", "After scale $normalized")
+        normalized = scale(shifted)
+//        Log.d("UdpManager", "After scale $normalized")
 
-        return Triple(isLeftSensor, normalized, columnHit )
+        Log.d("UdpManager", "returns:${Triple(isLeftSensor, normalized, columnHit)}")
+//        return Triple(isLeftSensor, normalized, columnHit )
     }
 
-    fun wrapAroundBase(angleZ: Double): Double {
+    fun wrapAroundBase(angleZTemp: Double): Double {
         // Shifts 0 point to the given angle_z_base without changing domain <-180, 180>
-        var yaw = angleZ - baseAngleZ
+        var yaw = angleZTemp - baseAngleZ
         if (yaw < -180) yaw += 360
         if (yaw > 180) yaw -= 360
         return yaw;
     }
 
     fun scale(angle: Double): Double {
-        val normalized = ( (-angle-MIN_RANGE) / (MAX_RANGE-MIN_RANGE) ) * 2.0
+        val normalized = ((-angle - MIN_RANGE) / (MAX_RANGE - MIN_RANGE)) * 2.0
         val scaled = normalized.coerceIn(0.0, 2.0)
         return scaled - 1.0
 //        var clamped = (-angle).coerceIn(MIN_RANGE, MAX_RANGE)
